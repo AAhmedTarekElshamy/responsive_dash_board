d97059bfa72d4a77b07083b5c52b4ff93dfb8853
Ahmed Tarek <ahmedtarik20188@gmail.com>
2025-06-12 22:22:29 +0300

    first commit

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/size_config.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
+
+import '../widgets/dashboard_desktop_layout.dart';
+
+
+class DashBoradView extends StatefulWidget {
+  const DashBoradView({super.key});
+
+  @override
+  State<DashBoradView> createState() => _DashBoradViewState();
+}
+
+class _DashBoradViewState extends State<DashBoradView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return Scaffold(
+
+      backgroundColor: const Color(0xFFF7F9FA),
+
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) =>const SizedBox(),
+        desktopLayout: (context) => const DashboardDesktopLayout(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class DashboardDesktopLayout extends StatelessWidget {
+  const DashboardDesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
