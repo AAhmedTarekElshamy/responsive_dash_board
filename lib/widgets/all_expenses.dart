9caf40885e8d4e55e21e6c8560e0f5d9ba1440f1
Ahmed Tarek <ahmedtarik20188@gmail.com>
2025-07-11 06:19:29 +0300

    AllExpensesHeader  ✅

---
+++ lib/widgets/all_expenses.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'all_expenses_header.dart';
+class AllExpenses extends StatelessWidget {
+  const AllExpenses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+
+     Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration:ShapeDecoration(
+          shape:RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ) ,
+          color: Colors.white,
+
+        ) ,
+       child: const Column(
+         children: [
+           AllExpensesHeader(),
+         ],
+       ),
+
+
+
+
+    );
+
+  }
+}
